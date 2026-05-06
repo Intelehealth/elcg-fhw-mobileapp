b1a6250e2190be480898135e200ebc29c6b759a3
hatanvirintele <mhasan@intelehealth.org>
2026-05-06 15:29:19 +0600

    (EZ - 805) Patient shifting issue fixes - query updated

--- app/src/main/java/org/intelehealth/ezazi/database/dao/ProviderDAO.java
+++ app/src/main/java/org/intelehealth/ezazi/database/dao/ProviderDAO.java
@@ -282,12 +282,12 @@ public class ProviderDAO {
                         "where p.role='Organizational: Nurse'" +
                         "and (pa.value is null or pa.value != 'Post Natal Ward') order by pa.value desc";
             }else if(ward.equals("Post Natal Ward") && isFromHome){
-                query = "select p.*, pa.* from tbl_provider as p " +
+                query = "select p.* from tbl_provider as p " +
                         "left join tbl_provider_attribute as pa on  p.uuid = pa.provideruuid " +
                         "where p.role='Organizational: Nurse'" +
                         "and (pa.value is null or pa.value != 'Labor Ward') order by pa.value desc";
             }else if(ward.equals("Labor Ward") && !isFromHome){
-                query = "select p.*, pa.* from tbl_provider as p " +
+                query = "select p.* from tbl_provider as p " +
                         "left join tbl_provider_attribute as pa on  p.uuid = pa.provideruuid " +
                         "where p.role='Organizational: Nurse'" +
                         "and (pa.value is null or pa.value != 'Labor Ward') order by pa.value desc";
