0337ba6375a68fdf21ba230e3fe87133bc36e1fe
hatanvirintele <mhasan@intelehealth.org>
2026-06-11 15:32:01 +0600

    (EZ-709) privacy accept and reject click event issue resolved

--- app/src/main/java/org/intelehealth/ezazi/activities/privacyNoticeActivity/PrivacyNoticeActivity.java
+++ app/src/main/java/org/intelehealth/ezazi/activities/privacyNoticeActivity/PrivacyNoticeActivity.java
@@ -121,7 +121,7 @@ public class PrivacyNoticeActivity extends BaseActionBarActivity implements View
             Log.d("Privacy", "selected radio: " + accept.getText().toString());
             startActivity(intent);
             finish();
-        } else if (checkBox_cho.isChecked() && v.getId() == R.id.button_reject) {
+        } else if (checkBox_cho.isChecked()) {
             Toast.makeText(PrivacyNoticeActivity.this,
                     getString(R.string.privacy_reject_toast), Toast.LENGTH_SHORT).show();
             finish();
